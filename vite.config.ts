9478694c957d7dfcba9a759b67c9a9c6d9435a2a
smba11 <simba.1w34@gmail.com>
2026-04-26 19:51:04 -0700

    Fix GitHub Pages blank page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import { defineConfig } from "vite"
 export default defineConfig({
   base: "/AmanAsmelash/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "src/index.template.html"),
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "."),
